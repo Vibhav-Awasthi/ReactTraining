ca725d6a272e3cf9cebe9ccad7b704864a5dc91b
Vibhav-Awasthi <vibhav.awasthi@appinventiv>
2021-06-02 10:29:39 +0530

    Home Component Added

--- src/modules/index.js
+++ src/modules/index.js
@@ -2,12 +2,13 @@
 //component define, declare
 //html,js
 import React, {Component} from 'react';
+import './index.css'
 
 class Home extends Component{
     render(){
         return(
-            <div>
-                <h1>{"Hello from Home"}</h1>
+            <div className={"homeContainer"}>
+                <h1 id={""}>{"Hello from Home"}</h1>
             </div>
         );
     }
