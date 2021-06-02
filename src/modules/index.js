f9233ccafebbd503086ae3dbb4b19bc5b3f5e9db
appinventiv <vibhav.awasthi@appinventiv>
2021-06-02 09:57:51 +0530

    added home component

---
+++ src/modules/index.js
@@ -0,0 +1,16 @@
+//imports
+//component define, declare
+//html,js
+import React, {Component} from 'react';
+
+class Home extends Component{
+    render(){
+        return(
+            <div>
+                <h1>{"Hello from Home"}</h1>
+            </div>
+        );
+    }
+}
+
+export default Home;
